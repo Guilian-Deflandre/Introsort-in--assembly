09cdd065042d852e0fd6fabb49ae7e83ba4b6580
Guilian Deflandre <55530877+Guilian-Deflandre@users.noreply.github.com>
2020-02-20 16:24:23 +0100

    Update heapsort.asm

--- Code/heapsort.asm
+++ Code/heapsort.asm
@@ -4,40 +4,40 @@
 heapsort:
   PUSH(LP)
   PUSH(BP)
-	MOVE(SP,BP)
+  MOVE(SP,BP)
 
   |; ***** Arguments *****
-	PUSH(R1)
+  PUSH(R1)
   PUSH(R2)
-	LD(BP, -12, R1) 		               |; R1 = array
-	LD(BP, -16, R2) 		               |; R2 = size
+  LD(BP, -12, R1) 		               |; R1 = array
+  LD(BP, -16, R2) 		               |; R2 = size
 
   |; ***** Local variables *****
   |; NB: For all our implementations, we choose to declare locals variables
   |; just after the initialisation of our arguments. This could lead to minimal
   |; non-optimalities of memory management but it simplifies the process ending
   |; of each function (by simply POP used registers at the end).
-	PUSH(R3)
-	DIVC(R2, 2, R3) 		               |; R3 = size/2
-	SUBC(R3, 1, R3)		                 |; R3 = i = size/2 - 1
+  PUSH(R3)
+  DIVC(R2, 2, R3) 		               |; R3 = size/2
+  SUBC(R3, 1, R3)		                 |; R3 = i = size/2 - 1
 
 loop1:
-	CMPLE(R31, R3, R0)                 |; Cannot use CMPLTC in reverse order
+  CMPLE(R31, R3, R0)                 |; Cannot use CMPLTC in reverse order
   BF(R0, end_loop1)
   PUSH(R3)       				             |; i
   PUSH(R2)       				             |; size
-	PUSH(R1)       	  			           |; array
+  PUSH(R1)       	  			           |; array
   CALL(heapify, 3)     	             |; heapify(array, size, i)
   SUBC(R3, 1, R3)	    	             |; R3 = i - 1
   BR(loop1)
 
 end_loop1:
-	SUBC(R2, 1, R3)	    	             |; R3 = size - 1 = i
+  SUBC(R2, 1, R3)	    	             |; R3 = size - 1 = i
 
 loop2:
-	CMPLE(R31, R3, R0)                 |; Cannot use CMPLTC in reverse order
+  CMPLE(R31, R3, R0)                 |; Cannot use CMPLTC in reverse order
   BF(R0, heapsort_end)
-	ADDR(R1, R3, R0)       	           |; R0 = array + 4 * i (valid address)
+  ADDR(R1, R3, R0)       	           |; R0 = array + 4 * i (valid address)
 
   |; We need new registers to perform the swap
   PUSH(R4)
@@ -51,47 +51,47 @@ loop2:
   PUSH(R1)            	             |; array
   CALL(heapify, 3)   	               |; heapify(array, i, 0)
   SUBC(R3, 1, R3)	    	             |; R3 = i = i-1
-	BR(loop2)
+  BR(loop2)
 
 heapsort_end:
-	POP(R3)
+  POP(R3)
   POP(R2)
   POP(R1)
-	MOVE(BP, SP)
-	POP(BP)
+  MOVE(BP, SP)
+  POP(BP)
   POP(LP)
-	RTN()
+  RTN()
 
 heapify:
- 	PUSH(LP)
-	PUSH(BP)
-	MOVE(SP, BP)
+  PUSH(LP)
+  PUSH(BP)
+  MOVE(SP, BP)
 
   |; ***** Arguments *****
-	PUSH(R1)
+  PUSH(R1)
   PUSH(R2)
   PUSH(R3)
-	LD(BP, -12, R1) 		               |; R1 = array
-	LD(BP, -16, R2) 		               |; R2 = size
-	LD(BP, -20, R3) 		               |; R3 = index
+  LD(BP, -12, R1) 		               |; R1 = array
+  LD(BP, -16, R2) 		               |; R2 = size
+  LD(BP, -20, R3) 		               |; R3 = index
 
   |; ***** Local variables *****
   |; NB: For all our implementations, we choose to declare locals variables
   |; just after the initialisation of our arguments. This could lead to minimal
   |; non-optimalities of memory management but it simplifies the process ending
   |; of each function (by simply POP used registers at the end).
-	PUSH(R4) 		                       |; R4 = largest
-	PUSH(R5) 	                       	 |; R5 = left
-	PUSH(R6) 		                       |; R6 = right
+  PUSH(R4) 		                       |; R4 = largest
+  PUSH(R5) 	                       	 |; R5 = left
+  PUSH(R6) 		                       |; R6 = right
 
 while_loop_heapify:
-	CMPLT(R3, R2, R0)
+  CMPLT(R3, R2, R0)
   BF(R0, heapify_end)
-	MOVE(R3, R4)  	 	                 |; largest = index
-	MULC(R3, 2, R0) 		               |; R0 = index*2
-	ADDC(R0, 1, R5) 		               |; R5 = left = index*2 + 1
-	ADDC(R3, 1, R0) 		               |; R0 = index + 1
-	MULC(R0, 2, R6) 		               |; R6 = right = (index + 1)*2
+  MOVE(R3, R4)  	 	                 |; largest = index
+  MULC(R3, 2, R0) 		               |; R0 = index*2
+  ADDC(R0, 1, R5) 		               |; R5 = left = index*2 + 1
+  ADDC(R3, 1, R0) 		               |; R0 = index + 1
+  MULC(R0, 2, R6) 		               |; R6 = right = (index + 1)*2
 
   CMPLT(R5, R2, R0)   	             |; R0 = left < size
 
@@ -111,7 +111,7 @@ while_loop_heapify:
   MOVE(R5, R4)     	                 |; largest = left
 
 if2_heapify:
-	CMPLT(R6,R2,R0)    	               |; R0 = right < size
+  CMPLT(R6,R2,R0)    	               |; R0 = right < size
 
   |; We need register for the double comparison
   PUSH(R7)
@@ -129,13 +129,13 @@ if2_heapify:
   MOVE(R6, R4)        	             |; largest = right
 
 if3_heapify:
-	CMPEQ(R3, R4, R0)                  |; largest != index check
+  CMPEQ(R3, R4, R0)                  |; largest != index check
   BT(R0, heapify_end)
 
   |; left and right are no more use until now in the code so we can reuse the
   |; registers countaining their values.
-	ADDR(R1, R4, R5) 	   	             |; R5 = array+ 4 * largest
-	ADDR(R1, R3, R6) 	   	             |; R6 = array+ 4 * index
+  ADDR(R1, R4, R5) 	   	             |; R5 = array+ 4 * largest
+  ADDR(R1, R3, R6) 	   	             |; R6 = array+ 4 * index
 
   |; We need new registers to perform the swap operation
   PUSH(R7)
@@ -153,7 +153,7 @@ heapify_end:
   POP(R3)
   POP(R2)
   POP(R1)
-	MOVE(BP, SP)
-	POP(BP)
+  MOVE(BP, SP)
+  POP(BP)
   POP(LP)
-	RTN()
+  RTN()
